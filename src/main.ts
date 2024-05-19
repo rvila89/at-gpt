f103a92e93adc0f0786a1273069c710438427f2f
ATMIRA\roger.vila <roger.vila@atmira.com>
2024-05-19 11:56:40 +0200

    fix: enablecors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import {NestFactory} from '@nestjs/core'
 import {AppModule} from './app.module'
 import {ValidationPipe} from '@nestjs/common'
 import * as bodyParser from 'body-parser'
+import {NextFunction, Response} from 'express'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -9,6 +10,23 @@ async function bootstrap() {
     origin: 'https://at-gpt-frontend.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept'
+  })
+
+  // Middleware para verificar cabeceras CORS
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    res.header(
+      'Access-Control-Allow-Origin',
+      'https://at-gpt-frontend.vercel.app'
+    )
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
+    )
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept')
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end()
+    }
+    next()
   })
 
   app.useGlobalPipes(
