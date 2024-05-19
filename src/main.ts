dc1895e2b0870e8a7116fd22b2eeb0cf82fd8174
ATMIRA\roger.vila <roger.vila@atmira.com>
2024-05-19 11:43:55 +0200

    fix: update cors credentials

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   app.enableCors({
     origin: 'https://at-gpt-frontend.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true
+    allowedHeaders: 'Content-Type, Accept'
   })
 
   await app.listen(3000)
