7a8f919581b352439488379ce34ce0cd42dd6c7e
ATMIRA\roger.vila <roger.vila@atmira.com>
2024-05-19 11:10:13 +0200

    fix: update database destPath to deploy in vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,39 @@ import {Module} from '@nestjs/common'
 
 import {ConfigModule} from '@nestjs/config'
 
-import {ormConfig} from '../ormConfig'
+// import {ormConfig} from '../ormConfig'
 import {AtGptChatModule} from './at-gpt-chat/at-gpt-chat.module'
 import {AtGptDatabaseModule} from './at-gpt-database/at-gpt-database.module'
 import {TypeOrmModule} from '@nestjs/typeorm'
+import {join} from 'path'
+import * as fs from 'fs'
+import {Persona} from './at-gpt-database/entities/persona.entity'
+import {Educacion} from './at-gpt-database/entities/educacion.entity'
+import {Idioma} from './at-gpt-database/entities/idioma.entity'
+import {Skill} from './at-gpt-database/entities/skill.entity'
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(ormConfig),
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => {
+        const srcPath = join(__dirname, 'data', 'atdb03_bck.db')
+        const destPath = '/tmp/atdb03_bck.db'
+
+        // Copia el archivo de la base de datos a /tmp si no existe
+        if (!fs.existsSync(destPath)) {
+          fs.mkdirSync('/tmp', {recursive: true})
+          fs.copyFileSync(srcPath, destPath)
+        }
+
+        return {
+          type: 'sqlite',
+          database: destPath, // Usa la base de datos en /tmp
+          entities: [Persona, Educacion, Idioma, Skill],
+          synchronize: true
+        }
+      }
+    }),
     AtGptChatModule,
     AtGptDatabaseModule
   ]
